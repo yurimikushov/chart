3e4e94178077a2c7f5014acaf98554162156997b
Yuri Mikushov <mikushov.yu@gmail.com>
2021-04-10 20:16:02 +0300

    chore: fixed code style

--- src/ColumnChart.js
+++ src/ColumnChart.js
@@ -5,19 +5,25 @@ class ColumnChart {
     this.data = data
     this.size = size
 
-    this.topPadding = this.size.height * 0.2
-    this.bottomPadding = this.size.height * 0.1
-    this.leftPadding = this.size.width * 0.01
-    this.rightPadding = this.size.width * 0.01
+    this.paddings = {
+      top: this.size.height * 0.2,
+      bottom: this.size.height * 0.1,
+      left: this.size.width * 0.01,
+      right: this.size.width * 0.01,
+    }
 
-    this.chartHeight = this.size.height - this.topPadding - this.bottomPadding
-    this.chartWidth = this.size.width - this.leftPadding - this.rightPadding
+    this.chartSize = {
+      height: this.size.height - this.paddings.top - this.paddings.bottom,
+      width: this.size.width - this.paddings.left - this.paddings.right,
+    }
 
-    this.backgroundColor = '#fff'
-    this.titleColor = '#000'
-    this.axisColor = '#00000050'
+    this.colors = {
+      background: '#fff',
+      title: '#000',
+      axis: '#00000050',
+    }
 
-    this.fontSize = {
+    this.fontSizes = {
       title: 18,
     }
 
@@ -52,31 +58,31 @@ class ColumnChart {
   }
 
   fillBackground() {
-    this.ctx.fillStyle = this.backgroundColor
+    this.ctx.fillStyle = this.colors.background
     this.ctx.fillRect(0, 0, this.size.width, this.size.height)
   }
 
   drawTitle() {
-    const topPadding = (this.topPadding - this.fontSize.title) / 2
+    const topPadding = (this.paddings.top - this.fontSizes.title) / 2
 
-    this.ctx.fillStyle = this.titleColor
-    this.ctx.font = `${this.fontSize.title}px serif`
+    this.ctx.fillStyle = this.colors.title
+    this.ctx.font = `${this.fontSizes.title}px serif`
     this.ctx.textAlign = 'center'
     this.ctx.fillText(
       this.title,
       this.size.width / 2,
-      this.fontSize.title + topPadding
+      this.fontSizes.title + topPadding
     )
   }
 
   drawYAxises() {
     const axisCount = 5
-    const axisesGap = this.chartHeight / axisCount
+    const axisesGap = this.chartSize.height / axisCount
 
     for (let i = 0; i <= axisCount; i++) {
       this.drawYAxis({
         x: 0,
-        y: i * axisesGap + this.topPadding,
+        y: i * axisesGap + this.paddings.top,
         width: this.size.width,
       })
     }
@@ -86,19 +92,19 @@ class ColumnChart {
     this.ctx.beginPath()
     this.ctx.moveTo(x, y)
     this.ctx.lineTo(width, y)
-    this.ctx.strokeStyle = this.axisColor
+    this.ctx.strokeStyle = this.colors.axis
     this.ctx.stroke()
   }
 
   drawXAxises() {
     const axisCount = this.data.length
-    const axisesGap = this.chartWidth / axisCount
+    const axisesGap = this.chartSize.width / axisCount
 
     for (let i = 0; i <= axisCount; i++) {
       this.drawXAxis({
-        x: i * axisesGap + this.leftPadding,
-        y: this.topPadding - 5,
-        height: this.chartHeight + this.topPadding + 5,
+        x: i * axisesGap + this.paddings.left,
+        y: this.paddings.top - 5,
+        height: this.chartSize.height + this.paddings.top + 5,
       })
     }
   }
@@ -107,7 +113,7 @@ class ColumnChart {
     this.ctx.beginPath()
     this.ctx.moveTo(x, y)
     this.ctx.lineTo(x, height)
-    this.ctx.strokeStyle = this.axisColor
+    this.ctx.strokeStyle = this.colors.axis
     this.ctx.stroke()
   }
 
@@ -119,17 +125,17 @@ class ColumnChart {
     const values = this.data.map((column) => column.value)
     const maxValue = Math.max(...values)
 
-    const gap = this.chartWidth * 0.02
+    const gap = this.chartSize.width * 0.02
 
     for (let i = 0; i < this.data.length; i++) {
       const { label, value, color } = this.data[i]
-      const columnHeight = value / (maxValue / this.chartHeight)
-      const columnWidth = this.chartWidth / values.length
+      const columnHeight = value / (maxValue / this.chartSize.height)
+      const columnWidth = this.chartSize.width / values.length
 
       this.ctx.fillStyle = color
       this.ctx.fillRect(
-        columnWidth * i + gap / 2 + this.leftPadding,
-        this.chartHeight - columnHeight + this.topPadding,
+        columnWidth * i + gap / 2 + this.paddings.left,
+        this.chartSize.height - columnHeight + this.paddings.top,
         columnWidth - gap,
         columnHeight
       )
