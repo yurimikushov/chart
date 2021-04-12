becc73855c15ae9a382a133132098fba9957e818
Yuri Mikushov <mikushov.yu@gmail.com>
2021-04-12 08:47:38 +0300

    chore: added another column chart example

--- public/app.js
+++ public/app.js
@@ -1,9 +1,19 @@
 import { ColumnChart } from '../src'
 import predictedWorldPopulation from './data/predictedWorldPopulation.json'
+import sales from './data/sales.json'
 
 new ColumnChart({
   title: 'Predicted world population (millions) in 2050',
   data: predictedWorldPopulation,
+  size: {
+    height: 400,
+    width: 600,
+  },
+}).mount(document.querySelector('.chart-container'))
+
+new ColumnChart({
+  title: 'Sales, $',
+  data: sales,
   size: {
     height: 400,
     width: 600,
